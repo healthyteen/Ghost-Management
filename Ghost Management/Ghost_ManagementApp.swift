e6c6fffe3953cca4d40ba0a40e78e2eb5950b81a
healthyteen <118077323+healthyteen@users.noreply.github.com>
2022-12-21 16:50:19 -0800

    Initial Commit

---
+++ Ghost Management/Ghost_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ghost_ManagementApp.swift
+//  Ghost Management
+//
+//  Created by Skyler Szijjarto on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Ghost_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
